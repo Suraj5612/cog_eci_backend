2af37eda22ac2efc53f09b6d1d2bd7d9b7d39b0b
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-24 23:57:12 +0530

    added voter routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes.js";
+import voterRoutes from "./modules/voter/voter.routes.js";
 
 import { errorHandler } from "./middlewares/error.middleware.js";
 import { authMiddleware } from "./middlewares/auth.middleware.js";
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/voters", voterRoutes);
 
 app.get("/health", (req, res) => {
   res.json({ message: "OK" });
